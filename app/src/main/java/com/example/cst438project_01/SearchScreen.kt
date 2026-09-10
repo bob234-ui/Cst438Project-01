25462aa1aed165424d00e34ce4834ed5403edaa7
Braeden <bmigay@csumb.edu>
2026-09-09 17:02:24 -0700

    first commit for no-results

--- app/src/main/java/com/example/cst438project_01/SearchScreen.kt
+++ app/src/main/java/com/example/cst438project_01/SearchScreen.kt
@@ -4,23 +4,30 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview // So the layout can be seen
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import java.security.Key
 
 @Composable
 fun SearchScreen(onGoToPersonalPage: () -> Unit = {}) {
     // Tracks the current text entered in the search bar
     var query by remember {mutableStateOf("")}
+
+    var showNoResultsDialogue by remember {mutableStateOf(false)}
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -37,7 +44,11 @@ fun SearchScreen(onGoToPersonalPage: () -> Unit = {}) {
                 value = query, // The current value to display
                 onValueChange = { query = it }, // Updates the state when the user types
                 label = { Text("Search") },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                //creates onscreen keyboard
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+
+                keyboardActions = KeyboardActions(onSearch = {/*other code goes here*/ showNoResultsDialogue = true})
             )
         }
     }
