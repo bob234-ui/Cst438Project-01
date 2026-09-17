e157705a7d39bbe5ec8023f7ffc394bb0e3f14ab
Braeden <bmigay@csumb.edu>
2026-09-16 21:48:56 -0700

    created GreetingTest.kt

---
+++ app/src/androidTest/java/com/example/cst438project_01/GreetingTest.kt
@@ -0,0 +1,11 @@
+package com.example.cst438project_01
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import org.junit.Rule
+import org.junit.Test
+
+class GreetingTest {
+}
